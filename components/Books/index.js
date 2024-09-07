729a4f55e2a31e3438d69c6930bfe7eb0c034111
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2024-09-07 14:13:45 +0700

    Update index.js

--- components/Books/index.js
+++ components/Books/index.js
@@ -16,10 +16,19 @@ export const Books = () => {
     "The Two Towers",
     "The Return of the King",
     "The Silmarillion",
-    "The Silmarillion",
     "The Return of the King",
-    "The Return of the King",
-    "The Return of the King",
+    "Science",
+    "Technology",
+    "Computer Science",
+    "Mathematics",
+    "Physics",
+    "Chemistry",
+    "Biology",
+    "Geography",
+    "History",
+    "Art",
+    "Music",
+    "Philosophy",
   ];
   const index = Math.floor(Math.random() * random.length);
   useEffect(() => {
@@ -36,14 +45,14 @@ export const Books = () => {
       <main>
         <div className="grid md:grid-cols-4 grid-cols-2 gap-4 p-4">
           {data.map((datas, index) => (
-            <Link href={`/book/${datas?.id}`} key={index}>
+            <Link href={`/book/${datas?.id}`} key={index} className="md:px-2">
               <div className="relative group overflow-hidden rounded-lg shadow-lg">
                 <Image
                   src={datas?.imageUrl}
                   alt={datas?.title}
                   width={200}
                   height={300}
-                  className="w-full h-full object-cover"
+                  className="w-full max-h-64 object-cover"
                 />
                 <div className="absolute bottom-0 left-0 w-full bg-gradient-to-t from-black to-transparent p-4">
                   <h1 className="text-white text-lg font-semibold">
