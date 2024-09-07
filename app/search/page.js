a2f19e717838b08fcdf27acae29bf6a6401bf54f
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2024-09-07 14:15:35 +0700

    Update page.js

--- app/search/page.js
+++ app/search/page.js
@@ -3,7 +3,12 @@ import { SearchDataBooks } from "@/lib/data";
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { Poppins } from "next/font/google";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "800"],
+});
 const Page = ({ searchParams: { query } }) => {
   const [data, setData] = useState([]);
   const fetchData = async () => {
@@ -16,7 +21,7 @@ const Page = ({ searchParams: { query } }) => {
   }, [query]);
 
   return (
-    <main>
+    <main className={poppins.className}>
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold p-4">Result for: {query}...</h1>
         <Link href={`/`} className="underline text-yellow-500 font-bold p-4">
@@ -25,14 +30,14 @@ const Page = ({ searchParams: { query } }) => {
       </div>
       <div className="grid md:grid-cols-4 grid-cols-2 gap-4 p-4">
         {data.map((datas, index) => (
-          <Link href={`/book/${datas?.id}`} key={index}>
+          <Link href={`/book/${datas?.id}`} key={index} className="md:px-2">
             <div className="relative group overflow-hidden rounded-lg shadow-lg">
               <Image
                 src={datas?.imageUrl}
                 alt={datas?.title}
                 width={200}
                 height={300}
-                className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
+                className="w-full max-h-64 object-cover transition-transform duration-500 group-hover:scale-105"
               />
               <div className="absolute inset-0 bg-gradient-to-t from-black to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 p-4">
                 <h1 className="text-white text-lg font-semibold">
