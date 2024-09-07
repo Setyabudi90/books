ee95eb80fc931a8ec0e938ee2dcc6d0bd7a77c5c
Inggrit Setya Budi <115447070+Setyabudi90@users.noreply.github.com>
2024-09-07 14:15:04 +0700

    Update page.js

--- app/book/[id]/page.js
+++ app/book/[id]/page.js
@@ -1,12 +1,12 @@
 "use client";
 import { getDataById } from "@/lib/data";
 import Image from "next/image";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Page = ({ params: { id } }) => {
   const [data, setData] = useState();
+
   const fetchBookById = async () => {
     const books = await getDataById(id);
     setData(books);
@@ -16,50 +16,64 @@ const Page = ({ params: { id } }) => {
     fetchBookById();
   }, [id]);
 
+  const validateDescription = (text) => {
+    if (text !== undefined) {
+      if (text?.length >= 501) {
+        return text.substring(0, 610) + "...";
+      }
+    }
+    return text;
+  };
+
   return (
-    <div className="flex flex-col md:gap-2 py-4 md:mx-4 mx-2">
-      <div className="grid md:grid-cols-1 gap-2 grid-cols-1">
-        <div className="flex flex-col justify-center items-center">
+    <div
+      className="flex flex-col md:gap-2 w-full"
+      style={{
+        background: "linear-gradient(135deg, #dddddd56, #0d161b)",
+        padding: "20px",
+      }}
+    >
+      <div className="grid md:grid-cols-2 gap-4 grid-cols-1">
+        <div className="flex justify-center items-center h-auto">
           <Image
             src={data?.imageUrl}
             alt={`${id}`}
             width={250}
             height={250}
-            className="md:w-[400px] md:h-[400px] rounded object-cover"
+            className="w-full max-h-[600px] rounded object-cover shadow-lg"
           />
         </div>
-        <div
-          className="flex md:justify-center justify-start
-items-center p-4 text-xl font-bold overflow-x-auto md:overflow-x-visible"
-        >
-          <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-indigo-500">
-            Authors: {data?.authors}
-          </span>
-          {data?.publisher ? (
-            <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-indigo-500">
-              Publisher: {data.publisher}
-            </span>
-          ) : (
-            ""
-          )}
-          <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-indigo-500">
-            Category: {data?.categories[0]}
-          </span>
-          <span className="whitespace-nowrap rounded-full bg-purple-100 px-2.5 py-0.5 text-sm text-indigo-500">
-            Popularity: {data?.pageCount}
-          </span>
-        </div>
-        <div className="flex flex-col gap-2 py-4 md:justify-center justify-start px-5">
-          <h3 className={`md:text-3xl text-xl text-color-primary font-bold`}>
-            {data?.title}
-          </h3>
+        <div className="flex flex-col justify-start items-start gap-4 text-white">
+          <div className="flex flex-col gap-2">
+            <span className="text-3xl font-bold">{data?.title}</span>
+            <div className="flex gap-2 flex-wrap">
+              <span className="whitespace-nowrap rounded-full bg-purple-400 px-2.5 py-0.5 text-sm">
+                Authors: {data?.authors}
+              </span>
+              {data?.publisher && (
+                <span className="whitespace-nowrap rounded-full bg-purple-400 px-2.5 py-0.5 text-sm">
+                  Publisher: {data.publisher}
+                </span>
+              )}
+              <span className="whitespace-nowrap rounded-full bg-purple-400 px-2.5 py-0.5 text-sm">
+                Category: {data?.categories[0]}
+              </span>
+              <span className="whitespace-nowrap rounded-full bg-purple-400 px-2.5 py-0.5 text-sm">
+                Popularity: {data?.pageCount}
+              </span>
+            </div>
+          </div>
+
           <p
-            className={`break-words text-justify`}
-            dangerouslySetInnerHTML={{ __html: data?.description }}
+            className="text-justify leading-relaxed max-w-xl"
+            dangerouslySetInnerHTML={{
+              __html: validateDescription(data?.description),
+            }}
           />
+
           <Link
             href={`${data?.infoLink}`}
-            className="underline text-yellow-500"
+            className="underline text-yellow-300"
           >
             Selengkapnya...
           </Link>
